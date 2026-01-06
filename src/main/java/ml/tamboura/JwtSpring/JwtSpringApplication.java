0217d22833942bb484da7a339aa7174e33097bda
mohmoh9 <moskytamb69@gmail.com>
2026-01-06 11:39:58 +0000

    first commit

---
+++ src/main/java/ml/tamboura/JwtSpring/JwtSpringApplication.java
@@ -0,0 +1,13 @@
+package ml.tamboura.JwtSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringApplication.class, args);
+	}
+
+}
